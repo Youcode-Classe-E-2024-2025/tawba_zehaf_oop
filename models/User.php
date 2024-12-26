f2e80caeabe2d2fabdd5a03624679949e10aeb56
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2024-12-26 16:32:48 +0100

    creation de class user

--- models/User.php
+++ models/User.php
@@ -1,10 +1,42 @@
 <?php
-class User{
-    
+class User {
+    private $conn;
+    private $table = "users";
 
+    public $id;
+    public $username;
+    public $email;
+    public $created_at;
 
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function read() {
+        $query = "SELECT * FROM " . $this->table;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function create() {
+        $query = "INSERT INTO " . $this->table . "
+            SET
+                username = :username,
+                email = :email";
+
+        $stmt = $this->conn->prepare($query);
+
+        // Sanitize inputs
+        $this->username = htmlspecialchars(strip_tags($this->username));
+        $this->email = htmlspecialchars(strip_tags($this->email));
+
+        // Bind parameters
+        $stmt->bindParam(":username", $this->username);
+        $stmt->bindParam(":email", $this->email);
+
+        return $stmt->execute();
+    }
 }
-
-
 
 ?>
