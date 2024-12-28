99f5407d1a232692a83dd19d46868a32fb1d8591
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2024-12-28 13:51:32 +0100

    adding the html for task edit

---
+++ views/task_edit.php
@@ -0,0 +1,5 @@
+<form method="post" action="">
+    <!-- other form fields -->
+    <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
+    <button type="submit">Submit</button>
+</form>
